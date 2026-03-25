50f15f5bdc29e2b5cb14f2a656472353072fec0d
oalt <oa@lacheweg21.de>
2026-03-25 11:54:16 +0100

    Initial code commit

---
+++ src/MDD4All.QVT.DataAccess.CycloneDX/CycloneDxJsonSerializer.cs
@@ -0,0 +1,20 @@
+﻿using CycloneDX.Json;
+using CycloneDX.Models;
+using LL.MDE.Components.Qvt.Common.DataModels;
+using System;
+using System.IO;
+
+namespace MDD4All.QVT.Transformations.CycloneDxTest1.Configuration
+{
+    public class CycloneDxJsonSerializer : IDomainJsonSerializer
+    {
+        public void SerializeToJsonFile(string path, Type type, object data)
+        {
+            if (data is Bom)
+            {
+                string json = Serializer.Serialize((Bom)data);
+                File.WriteAllText(path, json);
+            }
+        }
+    }
+}
